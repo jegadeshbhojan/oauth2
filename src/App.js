b3791770a0589242469e6132eadd02b9d0481f07
jegadeshbhojan <jegadeshin@gmail.com>
2022-01-15 22:27:54 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <a href="https://github.com/login/oauth/authorize">click here for login</a>
+    </div>
+  );
+}
+
+export default App;
